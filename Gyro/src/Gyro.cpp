6fc8468d38786569854b4b44504b467d584ae19c
Allan Wright <allanwright@comcast.net>
2021-09-12 22:34:34 -0700

    working on rotor model

--- Gyro/src/Gyro.cpp
+++ Gyro/src/Gyro.cpp
@@ -30,10 +30,8 @@ public:
 		Vec3 angVel(0);
 		MobilizedBodyIndex bi(1);
 		MobilizerUIndex ui(0);
-		const MobilizedBody& mobody1 = matter.getMobilizedBody(bi);
-		angVel = mobody1.getBodyAngularVelocity(state);	
-		printf("rpm = %.3f\r\n", 60*angVel[2]/(2*Pi));
-		mobilityForces[0] = 0.05;
+		const MobilizedBody& hub_mobody = matter.getMobilizedBody(bi);
+		mobilityForces[0] = 0.01;
 	}
 	Real calcPotentialEnergy(const State& state) const 
 	{
@@ -49,6 +47,26 @@ private:
 };
 
 //==============================================================================
+//                           EVENT REPORTER
+//==============================================================================
+class PositionReporter : public PeriodicEventReporter {
+public:
+	PositionReporter(const MultibodySystem& system, const MobilizedBody& mobod,
+		Real reportInterval)
+		: PeriodicEventReporter(reportInterval), system(system), mobod(mobod) {}
+	void handleEvent(const State& state) const {
+		system.realize(state, Stage::Position);   // so that variables can be accessed
+//		Vec3 pos = mobod.getBodyOriginLocation(state);
+		Vec3 angVel = mobod.getBodyAngularVelocity(state);
+		std::cout << state.getTime() << "\t" << angVel[2] << std::endl;
+	}
+private:
+	const MultibodySystem& system;
+	const MobilizedBody& mobod;
+};
+
+
+//==============================================================================
 //                                  MAIN
 //==============================================================================
 int main() {
@@ -59,22 +77,40 @@ int main() {
     SimbodyMatterSubsystem matter(system);
 	GeneralForceSubsystem forces(system);
 
-	// Constuct the rotor as a single rectangular shape
+	// Constuct the rotor from a cylindrical hub with a pin joint
+	// and multiple blades attached to the circumference of the hub
+	// with welds or hinges
+	// for now, assign moments of inertia for entire rotor to hub
 	Real density = 1000.0;	// density of rotor in kg/m^3
-	const Vec3 halfLengths(1.0, 0.055/2, 0.005/2);
-	Real rotor_mass = density * 2.* 0.055 * 0.0055;
-	Inertia rotor_MOI = rotor_mass * UnitInertia::brick(halfLengths);
-	Body::Rigid rotorBody(MassProperties(0.5, Vec3(0),
-							UnitInertia::brick(halfLengths)));
-	printf("Moments of inertia: %.4f, %.4f, %.4f\r\n", rotor_MOI.getMoments()[0],
-		rotor_MOI.getMoments()[1],
-		rotor_MOI.getMoments()[2]);
-	rotorBody.addDecoration(Transform(),
-		DecorativeBrick(halfLengths).setColor(Blue));
+	const Vec3 halfLengths(1.0, 0.055/2, 0.005/2);	// dimensions of hub + 2 blades
+	Real rotor_mass = density * 2.* 0.055 * 0.0055; // mass of rotor
+	Inertia hubI = rotor_mass * UnitInertia::brick(halfLengths); // MOI of rotor
+	Body::Rigid hub(MassProperties(0.5, Vec3(0),
+		UnitInertia::cylinderAlongZ(0.05, 0.01))); 
+//	UnitInertia::brick(halfLengths)));  //TODO
+	printf("Hub: Ixx %.4f, Iyy %.4f, Izz %.4f\r\n",
+		hubI.getMoments()[0],
+		hubI.getMoments()[1],
+		hubI.getMoments()[2]
+		);
+	// create decoration for the hub
+	Rotation R;
+	R.setRotationFromAngleAboutX(Pi/2);
+	Transform X(R);
+	hub.addDecoration(X, DecorativeCylinder(0.05, 0.01).setColor(Blue));
+	// create MobilizedBody for the hub
+	MobilizedBody::Pin hub_mobody(matter.Ground(), Transform(),
+		hub, Transform());
 
-	MobilizedBody::Pin rotor(matter.Ground(), Transform(),
-		rotorBody, Transform());
-	printf("Num mobodies %d\r\n", matter.getNumBodies());
+	// now create mobilized bodies for the blades
+	const Vec3 bladeHalfLengths(0.95/2, 0.055/2, 0.005/2);	// dimensions of blade
+	Real bladeMass = density * 0.95*0.055*0.005;
+	Inertia bladeI = bladeMass * UnitInertia::brick(bladeHalfLengths); // MOI of blade
+	Body::Rigid blade(MassProperties(bladeMass, Vec3(0),
+		UnitInertia::brick(bladeHalfLengths)));
+
+	// add event reporter, call every 0.1 sec
+	system.addEventReporter(new PositionReporter(system, hub_mobody, 0.1));
 
 	// Visualize a frame every 1/60 s, and include the energy.
     Visualizer viz(system); 
@@ -82,31 +118,32 @@ int main() {
     viz.addDecorationGenerator(new ShowData(system));
     system.addEventReporter(new Visualizer::Reporter(viz, 1./10));
 
-    // Initialize the system and state. 
+    // Initialize the state, including the custom Force 
 	Force::Custom(forces, new rotorTorque(matter));
-//	Force::DiscreteForces rotorDiscreteForce(forces, matter);
 	State state = system.realizeTopology();
+	RungeKuttaMersonIntegrator integ(system);
+	integ.setAccuracy(1e-5);
 
 	// set initial conditions
-	rotor.setQToFitTranslation(state, Vec3(0, 0, .5));
+//	rotor.setQToFitTranslation(state, Vec3(0, 0, .5));
 //	rotor.setUToFitAngularVelocity(state, Vec3(0, 0, 10)); // 10 rad/s
 	MobilizedBodyIndex bi(1);
 	MobilizerUIndex ui(0);
-	const MobilizedBody& mobody1 = matter.getMobilizedBody(bi);
-//	rotorDiscreteForce.setOneMobilityForce(state, mobody1, ui, 0.1);
-
-    // Simulate it.
-    RungeKuttaMersonIntegrator integ(system);
-    integ.setAccuracy(1e-5);
+	const MobilizedBody& mobody1 = hub_mobody;
+	
+	// Simulate it.
     TimeStepper ts(system, integ);
     ts.initialize(state);
+	ts.stepTo(10);
 
+#if 0
 	// set up loop
 	double timeStepSize = 0.01;
 	double timeTarget = timeStepSize;
 	double timeEnd = 10.0;
 	double timeCurrent = 0.0;
 	double torque = 0.0;
+
 	Vec3 angVel(0);
 	SpatialVec Vel6(0);
 	while (timeCurrent < timeEnd)
@@ -116,18 +153,11 @@ int main() {
 			// step until we reach timeTarget, in case stepTo() returns early
 			ts.stepTo(timeTarget);
 			timeCurrent = ts.getTime();
-			const MobilizedBody& mobody1 = matter.getMobilizedBody(bi);
-//			torque = rotorDiscreteForce.getOneMobilityForce(state, mobody1, ui);
-			//			angVel = body1.getBodyAngularVelocity(state);
-			//			Vel6 = mobody1.getMobilizerVelocity(state);
-//			angVel = rotor.getBodyAngularVelocity(state);
-			printf("%.3f %.3f ", timeCurrent, torque);
-			printf("\r\n");
 		}
 		// update rotor torque
-//		rotorDiscreteForce.clearAllMobilityForces(state);
 		timeTarget += timeStepSize;
 	}
+#endif
 	printf("done\r\n");
   } 
   catch (const std::exception& e)
@@ -140,13 +170,12 @@ int main() {
 
 
 //==============================================================================
-//                              SHOW DATA
+//                      ShowData::generateDecorations()
 //==============================================================================
-
 void ShowData::generateDecorations(const State&                state,
                                      Array_<DecorativeGeometry>& geometry)
 {
-	char s[10];
+	char s[20];
 	static int i = 0;
     m_mbs.realize(state, Stage::Dynamics);
     const Real E=m_mbs.calcEnergy(state);
@@ -157,7 +186,7 @@ void ShowData::generateDecorations(const State&                state,
             .setScale(.09)
             .setColor(Black);
 #endif
-	sprintf(s, "%3d", i);
+	sprintf(s, "   %3d", i);
 	energy.setText(s);
 	energy.setScale(.09);
 	energy.setColor(Black);
