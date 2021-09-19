4d6f5f57725f606308f1261df435a0c98dc46fd7
freepcb <allanwright@comcast.net>
2021-09-18 17:23:40 -0700

    Update Gyro.cpp

--- Gyro/src/Gyro.cpp
+++ Gyro/src/Gyro.cpp
@@ -30,7 +30,7 @@ RotorBlade bl(&af, bladeRootR, bladeTipR, bladeChord, bladePitch, bladeNsegs);
 //==============================================================================
 //                              SHOW DATA
 //==============================================================================
-// Generate text in the scene 
+// Generate text in the scene     kjlk
 class ShowData : public DecorationGenerator {
 public:
     explicit ShowData(const MultibodySystem& mbs) : m_mbs(mbs) {}
